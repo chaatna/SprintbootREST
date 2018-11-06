300afd9359a4f2da96a907d55861af62d98d3dbb
Maruthi Geethu Aadhanapattu <maruthigeethu@Maruthis-MacBook-Pro.local>
2018-11-05 22:37:55 -0500

    code cleanup and comments

--- src/main/java/com/comcast/test/Constants.java
+++ src/main/java/com/comcast/test/Constants.java
@@ -1,5 +1,10 @@
 package com.comcast.test;
 
+/**
+ * Constants for Test REST app
+ * @author - Chandra Sekhar Aadhanapattu
+ */
+
 public interface Constants {
 
     String TEST_END_POINT = "/hi";
